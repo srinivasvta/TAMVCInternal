f31a4324ea6f6b74e3f6789e97608a3800bfc9c4
srinivasvta <srinivasr@techaspect.com>
2016-04-25 16:34:31 +0530

    Test

--- TADigitalAds/App_Start/RouteConfig.cs
+++ TADigitalAds/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace TADigitalAds
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Default1",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
