fea33112133da3323da440974c7cb6935040b30f
madhukarr <madhukarr@techaspect.com>
2016-05-14 03:24:11 +0530

    Post Ad logic and Logini logic

--- TA.Classified/TA.Classified.Web/Controllers/LoginController.cs
+++ TA.Classified/TA.Classified.Web/Controllers/LoginController.cs
@@ -3,101 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using TA.Classified.BLL.ViewModels;
 using TA.Classified.Core;
 
 namespace TA.Classified.Web.Controllers
 {
-    public class LoginController : Controller
+  public class LoginController : Controller
+  {
+    public ActionResult Register()
     {
-        // GET: UserLogin
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: UserLogin/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: UserLogin/Create
-        public ActionResult Register()
-        {
-            return View();
-        }
-
-        // POST: UserLogin/Create
-        [HttpPost]
-        public ActionResult Register(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //Login//
-        [HttpGet]
-            public ActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Login(User user)
-        {
-            return View();
-        }
-
-        // GET: UserLogin/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: UserLogin/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: UserLogin/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: UserLogin/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+      return View();
     }
+
+    [HttpPost]
+    public ActionResult Register(FormCollection collection)
+    {
+      try
+      {
+        // TODO: Add insert logic here
+
+        return RedirectToAction("Index");
+      }
+      catch
+      {
+        return View();
+      }
+    }
+
+    public ActionResult Logout()
+    {
+      Session["UserEmail"] = null;
+      return RedirectToAction("Index", "Classified");
+    }
+
+    //Login//
+    [HttpGet]
+    public ActionResult Login()
+    {
+      UserLoginViewModel model = new UserLoginViewModel();
+      return View(model);
+    }
+
+    [HttpPost]
+    public ActionResult Login(UserLoginViewModel user)
+    {
+      if (BLL.BLLUser.AuthenticateUser(user).Equals(true))
+      {
+        Session["UserEmail"] = user.EmailAddress;
+        FormsAuthentication.SetAuthCookie(user.EmailAddress, true);
+      }
+      else
+      {
+        Session["UserEmail"] = null;
+        ViewBag.LoginMessage = "Login Failed..Please enter valid credentials";
+        return View(user);
+      }
+      return RedirectToAction("Index", "Classified");
+    }
+  }
 }
